66c500aee4ef44e8d418e5f982adcdd29664e2cd
abhijithpr08 <abhith628@gmail.com>
2025-12-23 06:21:13 +0530

    complete star rating project

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import { useState } from 'react'
+import './App.css'
+import StarRating from './components/star-rating'
+
+function App() {
+  return(
+    <div className="App">
+      <StarRating/>
+    </div>
+  )
+}
+
+export default App
